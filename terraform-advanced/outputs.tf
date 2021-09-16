5b3aff61877c1a63f415b5b94fe721722d2756bc
Varun Elavia <varun.elavia@aux.dkatalis.com>
2021-09-16 19:12:04 +0530

    changed output names

--- terraform-advanced/outputs.tf
+++ terraform-advanced/outputs.tf
@@ -1,28 +1,28 @@
-output "US_App_URL" {
+output "App_URL_US" {
   value = aws_apprunner_service.us.service_url
 }
-output "Asia_App_URL" {
+output "App_URL_Asia" {
   value = aws_apprunner_service.ap.service_url
 }
-output "Europe_App_URL" {
+output "App_URL_Europe" {
   value = aws_apprunner_service.eu.service_url
 }
-output "US_App_URL_simple_route" {
-  value = aws_route53_record.us-simple.name
+output "URL_for_simple_routing_US" {
+  value = "simple-us.${var.student_id}.cgpit.xyz"
 }
-output "Asia_App_URL_simple_route" {
-  value = aws_route53_record.ap-simple.name
+output "URL_for_simple_routing_Asia" {
+  value = "simple-us.${var.student_id}.cgpit.xyz"
 }
-output "Europe_App_URL_simple_route" {
-  value = aws_route53_record.eu-simple.name
+output "URL_for_simple_routing_Europe" {
+  value = "simple-us.${var.student_id}.cgpit.xyz"
 }
-output "URL_for_Geolocation_based_routing" {
+output "URL_for_geolocation_based_routing" {
   value = "geo.${var.student_id}.cgpit.xyz"
 }
-output "URL_for_Weighted_routing" {
+output "URL_for_weighted_routing" {
   value = "weighted.${var.student_id}.cgpit.xyz"
 }
-output "URL_for_Latency_based_routing" {
+output "URL_for_latency_based_routing" {
   value = "latency.${var.student_id}.cgpit.xyz"
 }
 output "URL_for_failover_routing" {
