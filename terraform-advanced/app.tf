b89423acb995eba9813aac1b193ac975d96b0042
Varun Elavia <varun.elavia@aux.dkatalis.com>
2021-09-16 17:17:40 +0530

    added domain associations

--- terraform-advanced/app.tf
+++ terraform-advanced/app.tf
@@ -53,6 +53,7 @@ resource "aws_apprunner_service" "eu" {
   }
 }
 
+## Domain Associations for Simple routes
 resource "aws_apprunner_custom_domain_association" "us-simple" {
   provider    = aws.us
   domain_name = "us-simple.${var.student_id}.cgpit.xyz"
@@ -66,5 +67,69 @@ resource "aws_apprunner_custom_domain_association" "ap-simple" {
 resource "aws_apprunner_custom_domain_association" "eu-simple" {
   provider    = aws.eu
   domain_name = "eu-simple.${var.student_id}.cgpit.xyz"
+  service_arn = aws_apprunner_service.eu.arn
+}
+## Domain Associations for Geolocation based routes
+resource "aws_apprunner_custom_domain_association" "us-geo" {
+  provider    = aws.us
+  domain_name = "geo.${var.student_id}.cgpit.xyz"
+  service_arn = aws_apprunner_service.us.arn
+}
+resource "aws_apprunner_custom_domain_association" "ap-geo" {
+  provider    = aws.ap
+  domain_name = "geo.${var.student_id}.cgpit.xyz"
+  service_arn = aws_apprunner_service.ap.arn
+}
+resource "aws_apprunner_custom_domain_association" "eu-geo" {
+  provider    = aws.eu
+  domain_name = "geo.${var.student_id}.cgpit.xyz"
+  service_arn = aws_apprunner_service.eu.arn
+}
+## Domain Associations for Weighted routes
+resource "aws_apprunner_custom_domain_association" "us-weighted" {
+  provider    = aws.us
+  domain_name = "weighted.${var.student_id}.cgpit.xyz"
+  service_arn = aws_apprunner_service.us.arn
+}
+resource "aws_apprunner_custom_domain_association" "ap-weighted" {
+  provider    = aws.ap
+  domain_name = "weighted.${var.student_id}.cgpit.xyz"
+  service_arn = aws_apprunner_service.ap.arn
+}
+resource "aws_apprunner_custom_domain_association" "eu-weighted" {
+  provider    = aws.eu
+  domain_name = "weighted.${var.student_id}.cgpit.xyz"
+  service_arn = aws_apprunner_service.eu.arn
+}
+## Domain Associations for Latency based routes
+resource "aws_apprunner_custom_domain_association" "us-latency" {
+  provider    = aws.us
+  domain_name = "latency.${var.student_id}.cgpit.xyz"
+  service_arn = aws_apprunner_service.us.arn
+}
+resource "aws_apprunner_custom_domain_association" "ap-latency" {
+  provider    = aws.ap
+  domain_name = "latency.${var.student_id}.cgpit.xyz"
+  service_arn = aws_apprunner_service.ap.arn
+}
+resource "aws_apprunner_custom_domain_association" "eu-latency" {
+  provider    = aws.eu
+  domain_name = "latency.${var.student_id}.cgpit.xyz"
+  service_arn = aws_apprunner_service.eu.arn
+}
+## Domain Associations for failover based routes
+resource "aws_apprunner_custom_domain_association" "us-failover" {
+  provider    = aws.us
+  domain_name = "failover.${var.student_id}.cgpit.xyz"
+  service_arn = aws_apprunner_service.us.arn
+}
+resource "aws_apprunner_custom_domain_association" "ap-failover" {
+  provider    = aws.ap
+  domain_name = "failover.${var.student_id}.cgpit.xyz"
+  service_arn = aws_apprunner_service.ap.arn
+}
+resource "aws_apprunner_custom_domain_association" "eu-failover" {
+  provider    = aws.eu
+  domain_name = "failover.${var.student_id}.cgpit.xyz"
   service_arn = aws_apprunner_service.eu.arn
 }
