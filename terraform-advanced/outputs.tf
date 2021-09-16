11451805d8d9fea5c7efbcf869f53daf595d0cd9
Varun Elavia <varun.elavia@aux.dkatalis.com>
2021-09-16 12:46:48 +0530

    fmt

--- terraform-advanced/outputs.tf
+++ terraform-advanced/outputs.tf
@@ -1,9 +1,9 @@
-output "US App URL" {
+output "US_App_URL" {
   value = aws_apprunner_service.us-west-2.service_url
 }
-output "Asia App URL" {
+output "Asia_App_URL" {
   value = aws_apprunner_service.ap-northeast-1.service_url
 }
-output "Europe App URL" {
+output "Europe_App_URL" {
   value = aws_apprunner_service.eu-west-1.service_url
 }
